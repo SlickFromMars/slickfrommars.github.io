8d88074bf3afd488befef6ebdad54193a2dee494
SlickFromMars <minecraftsummer2017@gmail.com>
2022-05-23 14:55:41 -0400

    BIG PROGRESS!

---
+++ scripts/nav_rep.js
@@ -0,0 +1,44 @@
+var target = document.getElementById('nav_target');
+
+// make the path prefix
+var pre_path = "../../";
+if(window.location.href.endsWith('index.html')) {
+    pre_path = "";
+}
+
+// make the bar
+target.classList.add("nav_bar");
+target.innerHTML = ""
+
+// the logo
+var icon = document.createElement("a");
+icon.href = pre_path + "index.html";
+
+img = document.createElement("img");
+img.classList.add("nav_logo");
+img.src = pre_path + "resources/logo.png";
+img.alt = "Home";
+
+icon.appendChild(img);
+
+target.appendChild(icon);
+
+// the buttons
+var tab_list = [
+    "Content",
+    "Foldverse",
+    "Music"
+];
+
+for (tab_name in tab_list) {
+    tab = document.createElement("button");
+    tab.classList.add("nav_tab");
+
+    tab_tex = document.createElement("a");
+    tab_tex.innerHTML = tab_list[tab_name];
+    tab_tex.href = pre_path + "pages/" + tab_list[tab_name].toLowerCase() + "/main.html";
+
+    tab.appendChild(tab_tex);
+
+    target.appendChild(tab);
+}
