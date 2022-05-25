6467374f3ca339e51a6f8ebca16d0a72b1f40bb3
SlickFromMars <minecraftsummer2017@gmail.com>
2022-05-25 09:02:20 -0400

    better stuff

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,12 +1,11 @@
 var target = document.getElementById('ProjectContainer');
 
 var data;
-var data2;
-var latest_data;
 
 var repo_list = [
     'Shredded-Plugin',
-    'Slick-Addons'
+    'Slick-Addons',
+    'ABLE-Tech'
 ];
 
 repo_list.forEach(function(repo) {
@@ -31,18 +30,11 @@ repo_list.forEach(function(repo) {
     view_tex.innerHTML = "View On Github";
     view_tex.href = data.html_url;
 
-    install = document.createElement("button");
-    install_tex = document.createElement("a");
-    install_tex.innerHTML = "Install Latest";
-    install_tex.href = latest_data.zipball_url;
-
     view.appendChild(view_tex);
-    install.appendChild(install_tex);
 
     tex.appendChild(title);
     tex.appendChild(desc);
     tex.appendChild(view);
-    tex.appendChild(install);
 
     main.appendChild(logo);
     main.appendChild(tex)
@@ -53,8 +45,6 @@ repo_list.forEach(function(repo) {
 function dataGet(repo)
 {
     data = JSON.parse(httpGet("https://api.github.com/repos/SlickFromMars/" + repo));
-    data2 = JSON.parse(httpGet("https://api.github.com/repos/SlickFromMars/" + repo + "/releases"));
-    latest_data = data2[0];
 }
 
 function httpGet(theUrl)
