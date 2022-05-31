70261f0db19ff82735deed39a16a8a2dec712435
SlickFromMars <minecraftsummer2017@gmail.com>
2022-05-31 09:07:28 -0400

    more stuff

--- scripts/foldverse.js
+++ scripts/foldverse.js
@@ -1,11 +1,13 @@
 var metaLink = "https://raw.githubusercontent.com/SlickFromMars/slickfrommars.github.io/main/meta/foldverse.json";
 
+var data;
+
 function foldverse_rep() {
     target = document.getElementById("MasterContainer");
 
     data = JSON.parse(httpGet(metaLink));
 
-    let keys = Object.keys(data);
+    let keys = Object.keys(data.main_page);
     keys.forEach(function(section) {
         let myData = data[section];
 
@@ -27,15 +29,24 @@ function foldverse_rep() {
             let comm2 = document.createComment(item);
             row_container.appendChild(comm2);
 
+            let rep_hold = document.createElement("a");
+            rep_hold.classList.add("rep_" + myData.format);
+            rep_hold.href = "preview.html#" + item;
+
             let rep = document.createElement("img");
-            rep.classList.add("rep_" + myData.format);
+            rep.id = item;
             rep.src = "../../resources/showcase/" + item + ".png";
 
-            row_container.appendChild(rep);
+            rep_hold.appendChild(rep);
+            row_container.appendChild(rep_hold);
         })
 
         container.appendChild(row_container);
 
         target.appendChild(container);
     })
+}
+
+function preview_rep() {
+    let content = window.location.hash.substring(1).replace(" ", "_");
 }
