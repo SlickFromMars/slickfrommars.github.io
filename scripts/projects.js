ef0b5eef91b096efa5d32db8ad2ea654f2298f31
SlickFromMars <minecraftsummer2017@gmail.com>
2022-06-06 14:12:59 -0400

    render moment

---
+++ scripts/projects.js
@@ -0,0 +1,101 @@
+var data = JSON.parse(httpGet("https://raw.githubusercontent.com/SlickFromMars/slickfrommars.github.io/main/meta/projects.json"))
+
+function project_rep() {
+    var target = document.getElementById('ProjectContainer');
+
+    data.repos.forEach(function(repo) {
+        let img_path = "../../resources/icon.webp";
+        let data = dataGet(repo);
+        
+        let comment = document.createComment(data.html_url);
+        target.appendChild(comment);
+
+        if(data.name == "Slick-Addons")
+            img_path = "../../resources/logo.webp"
+        else if(data.name == "Shredded-Plugin")
+            img_path = "../../resources/covers/sus.png"
+
+        main = document.createElement("div");
+        main.classList.add("container_grp");
+
+        logo = document.createElement("img");
+        logo.alt = repo;
+        logo.loading = "lazy";
+        logo.src = img_path;
+
+        tex = document.createElement("div");
+
+        title = document.createElement("h1");
+        title.innerHTML = data.name.replace("-", " ");
+
+        desc = document.createElement("h2");
+        desc.innerHTML = data.description;
+
+        view = document.createElement("button");
+        view_tex = document.createElement("a");
+        view_tex.innerHTML = "View On Github";
+        view_tex.href = data.html_url;
+
+        view.appendChild(view_tex);
+
+        tex.appendChild(title);
+        tex.appendChild(desc);
+        tex.appendChild(view);
+
+        main.appendChild(logo);
+        main.appendChild(tex);
+
+        target.appendChild(main);
+    });
+}
+
+function render_rep() {
+    var target = document.getElementById("RenderContainer");
+    var col1 = document.createElement("div");
+    col1.classList.add("column");
+    var col2 = document.createElement("div");
+    col2.classList.add("column");
+    var col3 = document.createElement("div");
+    col3.classList.add("column");
+
+    var swc = -1;
+
+    Object.keys(data.artwork).forEach(function(render) {
+        if(swc == 2) {
+            swc = 0;
+        } else {
+            swc = swc + 1;
+        }
+        let img_path ="../../resources/art/" + render + ".png";
+
+        let secondary;
+
+        if(data.artwork[render].author != undefined) {
+            secondary = document.createElement("h2");
+            secondary.innerHTML = data.artwork[render].author;
+        } else {
+            secondary = document.createComment("no author lol");
+        }
+
+        let img = document.createElement("img");
+        img.src = img_path;
+        img.alt = render;
+        if(swc == 0) {
+            col1.appendChild(img);
+            col1.appendChild(secondary);
+        } else if(swc == 1) {
+            col2.appendChild(img);
+            col2.appendChild(secondary);
+        } else {
+            col3.appendChild(img);
+            col3.appendChild(secondary);
+        }
+    })
+    target.appendChild(col1);
+    target.appendChild(col2);
+    target.appendChild(col3);
+}
+
+function dataGet(repo) {
+    return JSON.parse(httpGet("https://api.github.com/repos/SlickFromMars/" + repo));
+}
--- scripts/setup.js
+++ scripts/setup.js
@@ -4,30 +4,18 @@ var tab_list = [
     "Music"
 ];
 
-var repo_list = [
-    'Shredded-Plugin',
-    'Slick-Addons',
-    'ABLE-Tech'
-];
-
 if(document.location.href.includes("foldverse/preview.html") == false) {
     nav_rep();
 }
 
-if(document.location.href.endsWith("projects/main.html")) {
-    project_rep();
-}
-
 var pre_path;
-
 function nav_rep() {
-    var target = document.getElementById('nav_target');
-
-    // make the path prefix
     pre_path = "../../";
     if(window.location.href.endsWith('index.html')) {
         pre_path = "";
     }
+
+    var target = document.getElementById('nav_target');
 
     // make the bar
     target.classList.add("nav_bar");
@@ -67,62 +55,7 @@ function nav_rep() {
     })
 }
 
-function project_rep() {
-    var target = document.getElementById('ProjectContainer');
-
-    repo_list.forEach(function(repo) {
-        let img_path = "../../resources/icon.webp";
-        let data = dataGet(repo);
-        
-        let comment = document.createComment(data.html_url);
-        target.appendChild(comment);
-
-        if(data.name == "Slick-Addons")
-            img_path = "../../resources/logo.webp"
-        else if(data.name == "Shredded-Plugin")
-            img_path = "../../resources/covers/sus.png"
-
-        main = document.createElement("div");
-        main.classList.add("container_grp");
-
-        logo = document.createElement("img");
-        logo.alt = repo;
-        logo.loading = "lazy";
-        logo.src = img_path;
-
-        tex = document.createElement("div");
-
-        title = document.createElement("h1");
-        title.innerHTML = data.name.replace("-", " ");
-
-        desc = document.createElement("h2");
-        desc.innerHTML = data.description;
-
-        view = document.createElement("button");
-        view_tex = document.createElement("a");
-        view_tex.innerHTML = "View On Github";
-        view_tex.href = data.html_url;
-
-        view.appendChild(view_tex);
-
-        tex.appendChild(title);
-        tex.appendChild(desc);
-        tex.appendChild(view);
-
-        main.appendChild(logo);
-        main.appendChild(tex);
-
-        target.appendChild(main);
-    });
-}
-
-function dataGet(repo)
-{
-    return JSON.parse(httpGet("https://api.github.com/repos/SlickFromMars/" + repo));
-}
-
-function httpGet(theUrl)
-{
+window.httpGet = function(theUrl) {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", theUrl, false); // false for synchronous request
     xhr.send(null);
